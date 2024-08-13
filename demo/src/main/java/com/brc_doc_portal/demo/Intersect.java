e9d958bae32b87ac74ef33a57f29b08faf9a1ef4
UWILL18 <uwill18@wgu.edu>
2024-08-13 10:53:44 -0500

    brc_portal commences

--- demo/src/main/java/com/brc_doc_portal/demo/Intersect.java
+++ demo/src/main/java/com/brc_doc_portal/demo/Intersect.java
@@ -3,11 +3,14 @@ package com.brc_doc_portal.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//Isaiah 55:9
+//Exodus 13:21
+
 @SpringBootApplication
-public class DemoApplication {
+public class Intersect {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(Intersect.class, args);
 	}
 
 }
