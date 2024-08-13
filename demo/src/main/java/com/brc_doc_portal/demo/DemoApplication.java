bcd7c32ca9efdbae671827eaf377a48f7e44a88f
UWILL18 <uwill18@wgu.edu>
2024-08-12 22:41:57 -0500

    brc_portal commences

---
+++ demo/src/main/java/com/brc_doc_portal/demo/DemoApplication.java
@@ -0,0 +1,13 @@
+package com.brc_doc_portal.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
---
+++ demo/src/test/java/com/brc_doc_portal/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brc_doc_portal.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
